a96a15c2007afbf6e7bb1ddc7005a14cf1fb64b6
Maruf Bin Solaiman <0909maruf@gmail.com>
2024-09-11 04:05:56 +0600

    update navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,37 +1,4 @@
-// import React from 'react'
 
-// const Navbar = () => {
-//     return (
-//         <nav className="bg-white border-gray-200 shadow">
-//             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-//                 <a href="https://ayykori.net/" className="flex items-center space-x-3 rtl:space-x-reverse">
-//                     <img src="favicon.ico" className="h-8" alt="Ayykori Logo" />
-//                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-emerald-300">Ayykori Tasks</span>
-//                 </a>
-//                 <button data-collapse-toggle="navbar-default" type="button"
-//                     className="inline-flex justify-center items-center p-2 w-10 h-10 rounded-lg md:hidden hover:shadow text-emerald-300 hover:bg-emerald-100 " aria-controls="navbar-default" aria-expanded="false">
-//                     <span className="sr-only">Open main menu</span>
-//                     <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-//                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-//                     </svg>
-//                 </button>
-//                 <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-//                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white">
-//                         <li>
-//                             <a href="#" className="text-emerald-300 hover:text-emerald-400">Layout Builder</a>
-//                         </li>
-//                         <li>
-//                             <a href="#" className="text-emerald-300 hover:text-emerald-400">Tile Interaction</a>
-//                         </li>
-//                     </ul>
-//                 </div>
-//             </div>
-//         </nav>
-
-//     )
-// }
-
-// export default Navbar
 import React, { useState } from 'react';
 
 const Navbar = () => {
